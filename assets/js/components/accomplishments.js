0ff63bf3f37db669744563f1ddf9596953b6781c
Sagar Gupta <sagar.june97p@gmail.com>
2025-10-02 13:04:50 +0530

    feat: Added support for links in accomplishments section

--- assets/js/components/accomplishments.js
+++ assets/js/components/accomplishments.js
@@ -12,7 +12,8 @@ export const populateAccomplishments = (accomplishments) => {
                 ${accomplishments.map(acc => `
                     <li class="flex items-baseline text-lg text-gray-700 dark:text-gray-300">
                         <i class="fa-solid fa-trophy mr-3 text-primary-600"></i>
-                        <span class="flex-1">${acc}</span>
+                        <span class="flex-1">${acc.text}</span>
+                        ${acc.link ? `<a href="${acc.link}" target="_blank" rel="noopener noreferrer" class="ml-2 text-primary-600 hover:text-primary-dark"><i class="fas fa-external-link-alt"></i></a>` : ''}
                     </li>
                 `).join('')}
             </ul>
